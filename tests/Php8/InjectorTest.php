ca20848911e3dd8b3fa34250a32ae6e6b52cf3c8
roxblnfk <roxblnfk@ya.ru>
2020-07-09 16:17:00 +0300

    Add test with Union types

---
+++ tests/Php8/InjectorTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Injector\Tests\Php8;
+
+use DateTimeImmutable;
+use DateTimeInterface;
+use Yiisoft\Injector\Injector;
+use Yiisoft\Injector\MissingInternalArgumentException;
+use Yiisoft\Injector\Tests\Common\BaseInjectorTest;
+use Yiisoft\Injector\Tests\Common\Support\EngineInterface;
+use Yiisoft\Injector\Tests\Common\Support\EngineMarkTwo;
+use Yiisoft\Injector\Tests\Common\Support\EngineVAZ2101;
+use Yiisoft\Injector\Tests\Common\Support\MakeEngineMatherWithParam;
+use Yiisoft\Injector\Tests\Php8\Support\TimerUnionTypes;
+
+/**
+ * @requires PHP 8
+ */
+class InjectorTest extends BaseInjectorTest
+{
+    public function testMakeInternalClass(): void
+    {
+        $container = $this->getContainer();
+
+        $object = (new Injector($container))->make(\SplFileObject::class, [
+            'file_name' => __FILE__,
+            // second parameter skipped
+            // third parameter skipped
+            'context' => null,
+            'other-parameter' => true,
+        ]);
+
+        $this->assertSame(basename(__FILE__), $object->getFilename());
+    }
+
+    public function testInvokeUnionTypes(): void
+    {
+        $time = new DateTimeImmutable();
+        $container = $this->getContainer([DateTimeInterface::class => $time]);
+
+        $object = (new Injector($container))
+            ->make(TimerUnionTypes::class);
+
+        $this->assertSame($object->getTime(), $time);
+    }
+}
