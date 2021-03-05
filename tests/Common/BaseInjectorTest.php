ae599b0dc2acdb5cbce1618d903d986c792f9482
roxblnfk <roxblnfk@ya.ru>
2021-03-05 21:24:14 +0300

    Replace test container with SimpleContainer from yiisoft/test-support

--- tests/Common/BaseInjectorTest.php
+++ tests/Common/BaseInjectorTest.php
@@ -6,33 +6,12 @@ namespace Yiisoft\Injector\Tests\Common;
 
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
-use Psr\Container\NotFoundExceptionInterface;
+use Yiisoft\Test\Support\Container\SimpleContainer;
 
 abstract class BaseInjectorTest extends TestCase
 {
     protected function getContainer(array $definitions = []): ContainerInterface
     {
-        return new class($definitions) implements ContainerInterface {
-            private array $definitions;
-
-            public function __construct(array $definitions = [])
-            {
-                $this->definitions = $definitions;
-            }
-
-            public function get($id)
-            {
-                if (!$this->has($id)) {
-                    throw new class() extends \Exception implements NotFoundExceptionInterface {
-                    };
-                }
-                return $this->definitions[$id];
-            }
-
-            public function has($id): bool
-            {
-                return array_key_exists($id, $this->definitions);
-            }
-        };
+        return new SimpleContainer($definitions);
     }
 }
