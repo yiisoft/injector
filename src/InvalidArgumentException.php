d74677a07f9c497cf036b5e4f69047ab731a3f32
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-12-01 13:08:35 +0300

    PHP 8.5 support (#111)

--- src/InvalidArgumentException.php
+++ src/InvalidArgumentException.php
@@ -7,5 +7,5 @@ namespace Yiisoft\Injector;
 final class InvalidArgumentException extends ArgumentException
 {
     protected const EXCEPTION_MESSAGE = 'Invalid argument "%s" when calling "%s"%s. Non-interface argument should be'
-    . ' named explicitly when passed.';
+        . ' named explicitly when passed.';
 }
